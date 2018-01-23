8566ec98334870065d560239027b38498655fb4e
jorge <jlramirez1210@gmail.com>
2018-01-23 15:36:13 -0500

    punto 2 y 6

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Animacion de cambio de color
 function tituloBlanco(elemento){
   $('.main-titulo').animate(
     {
@@ -14,6 +15,41 @@ function tituloAmarillo(elemento){
       tituloBlanco(elemento);
     });
 }
+//Ordenar aleatoriamente los dulces
+function ordenarBloques(){
+  $('div[class ^= col]').each(function(item){
+    $(this).css('order',makeUniqueRandom());
+  })
+}
+var uniqueRandoms = [];
+var numRandoms = 49;
+function makeUniqueRandom() {
+  // refill the array if needed
+  if (!uniqueRandoms.length) {
+    for (var i = 0; i < numRandoms; i++) {
+      uniqueRandoms.push(i);
+    }
+  }
+  var index = Math.floor(Math.random() * uniqueRandoms.length);
+  var val = uniqueRandoms[index];
+  // now remove that value from the array
+  uniqueRandoms.splice(index, 1);
+  return val;
+}
+//boton de iniciar
+function iniciar(){
+  var txt = $('.btn-reinicio').text();
+  if(txt=='Iniciar'){
+    $('.elemento').css('visibility','visible');
+    $('.btn-reinicio').text("Reiniciar");
+  }else{
+    location.href = "index.html";
+  }
+}
 $(document).ready(function(){
   tituloBlanco($('.main-titulo'));
+  ordenarBloques();
+  $('.btn-reinicio').on('click', function(){
+    iniciar();
+  });
 });
